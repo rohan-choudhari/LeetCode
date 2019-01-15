56d16c385bf739389c32f23ae58a2c4ee480399f
rohan-choudhari <rohan.choudhari@hotmail.com>
2019-01-15 10:34:50 +0530

    maxSubarray reverseOnlyLetters

---
+++ reverseOnlyLetters.py
@@ -0,0 +1,45 @@
+"""
+Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+
+
+Example 1:
+
+Input: "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+
+
+Note:
+
+S.length <= 100
+33 <= S[i].ASCIIcode <= 122
+S doesn't contain \ or "
+"""
+
+class Solution:
+    def reverseOnlyLetters(self, S):
+        reversedS = S
+        curr_pointer = len(S)-1
+        for char in S:
+            if char.isalpha():
+                while(not((reversedS[curr_pointer]).isalpha())):
+                    curr_pointer=curr_pointer-1 
+                reversedS[curr_pointer] = char
+                curr_pointer=curr_pointer-1 
+        
+
+if 1:
+    S = "aa-bbbb-asdsda*zzzzz"
+obj = Solution()
+obj.reverseOnlyLetters(S)
+
+        
+       
